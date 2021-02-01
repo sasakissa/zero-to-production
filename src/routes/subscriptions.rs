1ea6ecd036eefcc05279093130779a740a8420fb
MitaRyosuke <sasakissa@gmail.com>
2021-02-01 17:03:11 +0900

    feat: 6.4

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,8 +1,9 @@
+use crate::domain::{NewSubscriber, SubscriberName};
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::{query, PgPool};
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
-
 #[derive(serde::Deserialize)]
 pub struct FormData {
     name: String,
@@ -18,22 +19,31 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
-    insert_subscriber(&pool, &form)
+    let name =
+        SubscriberName::parse(form.0.name).map_err(|_| HttpResponse::BadRequest().finish())?;
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name: name,
+    };
+    insert_subscriber(&pool, &new_subscriber)
         .await
         .map_err(|_| HttpResponse::InternalServerError().finish())?;
     tracing::info!("New subscriber details have been saved");
     Ok(HttpResponse::Ok().finish())
 }
 
-#[tracing::instrument(name = "Saving a new subscribers", skip(pool, form))]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+#[tracing::instrument(name = "Saving a new subscribers", skip(pool, new_subscriber))]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at)
             VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
